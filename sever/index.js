831986fcecd9a40b5d7f72bb2a3b796957e41a8d
v405 <valerieandy72@gmail.com>
2020-03-29 14:40:32 -0400

    Creating documentation collection

--- sever/index.js
+++ sever/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const monk = require('monk');
+const db = monk(localhost/Lilypad);
+const pads = db.get("pads");
 
 app.use(cors());
 app.use(express.json());
@@ -26,14 +28,19 @@ app.post('/pads', (req, res)=>{
             name: req.body.nane.toString(),
             content: req.body.content.toString()
         };
-        console.log(pad);
+
+        pads
+            .insert(pad)
+            .then(createdPad => {
+                res.json(createdPad)
+            });
     } else {
         res.status(422);
         res.json({
             message: "Alias and Content Required"
         });
     }
-});
+}); 
 
 app.listen(5000, () =>{
     console.log("Listening on http://localhost:5000")
