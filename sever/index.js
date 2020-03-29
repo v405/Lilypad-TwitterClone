bed918f4c2be6c798a4258645775b11653f33d60
v405 <valerieandy72@gmail.com>
2020-03-29 15:16:10 -0400

    messed up

--- sever/index.js
+++ sever/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const monk = require('monk');
-const db = monk(localhost/Lilypad);
+const db = monk('localhost/Lilypad');
 const pads = db.get("pads");
 
 app.use(cors());
